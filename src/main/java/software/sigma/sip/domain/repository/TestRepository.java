73ab7758fa13c3c396a9be70a9775dad2f9457c1
Roman Dovzhenko <dovzhenko.roman.kp03@gmail.com>
2022-08-20 19:49:49 +0300

    SIP-1:Create User entity class

--- src/main/java/software/sigma/sip/domain/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package software.sigma.sip.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import software.sigma.sip.domain.entity.Test;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-}
