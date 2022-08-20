9f1999e289d893cd3679a7ca93457fe39229422e
Roman Dovzhenko <dovzhenko.roman.kp03@gmail.com>
2022-08-20 20:02:34 +0300

    SIP-12:Create unit and integration tests

---
+++ src/test/java/software/sigma/sip/application/service/UserServiceTest.java
@@ -0,0 +1,114 @@
+package software.sigma.sip.application.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import software.sigma.sip.domain.entity.User;
+import software.sigma.sip.domain.repository.UserRepository;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+   @Mock
+   private UserRepository userRepository;
+   //@InjectMocks
+   private UserService userService;
+
+   @BeforeEach
+   public void setup() {
+      userService = new UserService(userRepository);
+   }
+
+   @Test
+   void getUsers() {
+      List<User> userList = Arrays.asList(
+              new User(1L, "Roman", "Dovzhenko", "romandovzhenko@gmail.com",
+                      "+380999999999", "Ukraine", "2003.03.14", "true"),
+              new User(2L, "Roman2", "Dovzhenko", "romandovzhenko@gmail.com",
+                      "+380999999999", "Ukraine", "2003.03.14", "true"));
+
+      Mockito.when(userRepository.findAll()).thenReturn(userList);
+      List<User> expectedList = userService.getUsers();
+
+      assertThat(expectedList).isEqualTo(userList);
+   }
+
+   @Test
+   void getUser_success() {
+      User sourceUser = new User(1L, "Roman", "Dovzhenko", "romandovzhenko@gmail.com",
+              "+380999999999", "Ukraine", "2003.03.14", "true");
+
+      Mockito.when(userRepository.findById(1L)).thenReturn(Optional.of(sourceUser));
+      User expectedUser = userService.getUser(1L);
+
+      assertThat(expectedUser).isEqualTo(sourceUser);
+   }
+
+   @Test
+   void getUser_failure() {
+      Mockito.when(userRepository.findById(1L)).thenReturn(Optional.empty());
+
+      Assertions.assertThrows(EntityNotFoundException.class, () -> userService.getUser(1L));
+   }
+
+   @Test
+   void addUser() {
+      User user = new User(1L, "Roman", "Dovzhenko", "romandovzhenko@gmail.com",
+              "+380999999999", "Ukraine", "2003.03.14", "true");
+
+      userService.addUser(user);
+
+      Mockito.verify(userRepository).save(user);
+   }
+
+   @Test
+   void updateUser_success() {
+      User user = new User(1L, "Roman", "Dovzhenko", "romandovzhenko@gmail.com",
+              "+380999999999", "Ukraine", "2003.03.14", "true");
+
+      Mockito.when(userRepository.existsById(1L)).thenReturn(true);
+      userService.updateUser(user);
+
+      Mockito.verify(userRepository).save(user);
+   }
+
+   @Test
+   void updateUser_failure() {
+      User user = new User(1L, "Roman", "Dovzhenko", "romandovzhenko@gmail.com",
+              "+380999999999", "Ukraine", "2003.03.14", "true");
+
+      Mockito.when(userRepository.existsById(1L)).thenReturn(false);
+
+      Assertions.assertThrows(EntityNotFoundException.class, () -> userService.updateUser(user));
+   }
+
+   @Test
+   void deactivateUser_success() {
+      User user = new User(1L, "Roman", "Dovzhenko", "romandovzhenko@gmail.com",
+              "+380999999999", "Ukraine", "2003.03.14", "true");
+
+      Mockito.when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+      Mockito.when(userRepository.existsById(1L)).thenReturn(true);
+      userService.deactivateUser(1L);
+
+      Mockito.verify(userRepository).save(new User(1L, "Roman", "Dovzhenko", "romandovzhenko@gmail.com",
+              "+380999999999", "Ukraine", "2003.03.14", "false"));
+   }
+
+   @Test
+   void deactivateUser_failure() {
+      Mockito.when(userRepository.findById(1L)).thenReturn(Optional.empty());
+
+      Assertions.assertThrows(EntityNotFoundException.class, () -> userService.deactivateUser(1L));
+   }
+}
