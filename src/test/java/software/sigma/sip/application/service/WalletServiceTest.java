f47cc8c87b9f7da15a109407720278a9ecf0260f
Ivan Baranetckiy <baranetckiy@gmail.com>
2022-08-22 19:49:18 +0300

    SIP-18: Remove directory in tests and change Service Test

--- src/test/java/software/sigma/sip/application/service/WalletServiceTest.java
+++ src/test/java/software/sigma/sip/application/service/WalletServiceTest.java
@@ -1,11 +1,11 @@
 package software.sigma.sip.application.service;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -21,14 +21,12 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class WalletServiceTest {
     @Mock
     private static WalletRepository walletRepository;
+    @InjectMocks
     private static WalletService walletService;
     @Captor
     ArgumentCaptor<Wallet> walletArgumentCaptor;
 
-    @BeforeEach
-    public void setup() {
-        walletService = new WalletService(walletRepository);
-    }
+
 
     @Test
     void addWallet_success() {
@@ -41,40 +39,37 @@ public class WalletServiceTest {
 
     @Test
     void getWallet_success() {
-        Long id = 1L;
         Wallet source = new Wallet(1L, 1L, "Ivan", "USD", "200.00", "17.08");
+
         Mockito.when(walletRepository.findById(1L))
                 .thenReturn(Optional.of(source));
-        Wallet actualWallet = walletService.getWallet(id);
+        Wallet actualWallet = walletService.getWallet(1L);
         assertThat(actualWallet).isEqualTo(source);
     }
 
     @Test
     void getWallet_failure() {
-        Long id = 1L;
-
         Mockito.when(walletRepository.findById(1L))
                 .thenReturn(Optional.empty());
 
-        Assertions.assertThrows(RuntimeException.class, () -> walletService.getWallet(id));
+        Assertions.assertThrows(RuntimeException.class, () -> walletService.getWallet(1L));
     }
 
     @Test
     void deleteWallet_success() {
         Long id = 1L;
         Mockito.when(walletRepository.existsById(id)).thenReturn(true);
-        walletService.deleteWallet(id);
+        walletService.deleteWallet(1L);
 
-        Mockito.verify(walletRepository).deleteById(id);
+        Mockito.verify(walletRepository).deleteById(1L);
     }
 
     @Test
     void deleteWallet_failure() {
-        Long id = 1L;
-        Mockito.when(walletRepository.existsById(id)).thenReturn(false);
+        Mockito.when(walletRepository.existsById(1L)).thenReturn(false);
         String expectedMessage = "Wallet was not found by id";
         String actualMessage = Assertions.assertThrows(RuntimeException.class, () ->
-                walletService.deleteWallet(id)).getMessage();
+                walletService.deleteWallet(1L)).getMessage();
         assertThat(expectedMessage).isEqualTo(actualMessage);
     }
 
@@ -82,14 +77,13 @@ public class WalletServiceTest {
     void updateWallet_success() {
         Wallet sourceWallet = new Wallet(1L, 2L, "Ivan", "USD", "200.00", "17.08");
         Wallet expected = new Wallet(1L, 2L, "Ivan", "USD", "200.00", "17.08");
-        Long id = 1L;
 
         Mockito.when(walletRepository.existsById(1L))
                 .thenReturn(true);
         Mockito.when(walletRepository.findById(1L))
                 .thenReturn(Optional.of(sourceWallet));
 
-        walletService.updateWallet(sourceWallet, id);
+        walletService.updateWallet(sourceWallet, 1L);
         Mockito.verify(walletRepository).save(walletArgumentCaptor.capture());
         assertThat(walletArgumentCaptor.getValue()).isEqualTo(expected);
 
@@ -104,7 +98,7 @@ public class WalletServiceTest {
         String expectedMessage = "Wallet was not found by id";
 
         String actualMessage = Assertions.assertThrows(RuntimeException.class, () ->
-                walletService.updateWallet(sourceWallet, id)).getMessage();
+                walletService.updateWallet(sourceWallet, 1L)).getMessage();
         assertThat(actualMessage).isEqualTo(expectedMessage);
     }
 
--- src/test/resources/db/migration/v00001__create_wallets_table.sql
+++
@@ -1,9 +0,0 @@
-create table wallet
-(
-    id         bigint auto_increment,
-    name       VARCHAR,
-    amount     VARCHAR,
-    created_at VARCHAR,
-    currency   VARCHAR,
-    owner_id   bigint,
-);
