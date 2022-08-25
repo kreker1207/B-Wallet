8355f28e74b7586f7c448171ce0e8961f5300be3
Roman Dovzhenko <dovzhenko.roman.kp03@gmail.com>
2022-08-25 14:16:57 +0300

    SIP-31: Extract Library Version to Properties. Extract url value to yaml file. Remove @JsonProperty annotation.

--- src/main/java/software/sigma/sip/domain/client/CurrencyDataFeedClient.java
+++ src/main/java/software/sigma/sip/domain/client/CurrencyDataFeedClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import software.sigma.sip.infrastructure.dto.CurrencyDto;
 
-@FeignClient(name = "currencyDataFeedClient", url = "https://currencydatafeed.com/api")
+@FeignClient(name = "currencyDataFeedClient", url = "${currency-api.url}")
 public interface CurrencyDataFeedClient {
    @GetMapping(value = "/source_currency.php")
    CurrencyDto getCourse(@RequestParam String source, @RequestParam String target, @RequestParam String token);
--- src/main/java/software/sigma/sip/infrastructure/dto/CurrencyDto.java
+++ src/main/java/software/sigma/sip/infrastructure/dto/CurrencyDto.java
@@ -1,6 +1,5 @@
 package software.sigma.sip.infrastructure.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,8 +11,6 @@ import java.util.Map;
 @Setter
 @AllArgsConstructor
 public class CurrencyDto {
-   @JsonProperty("status")
    private String status;
-   @JsonProperty("currency")
    private List<Map<String, String>> currency;
 }
