e2e819a38eb9978b5582ab62879e5b430b4eb854
Roman Dovzhenko <dovzhenko.roman.kp03@gmail.com>
2022-08-27 13:02:04 +0300

    SIP-41: Return wallets related to users in userDTO

--- src/main/java/software/sigma/sip/infrastructure/dto/UserDto.java
+++ src/main/java/software/sigma/sip/infrastructure/dto/UserDto.java
@@ -3,6 +3,9 @@ package software.sigma.sip.infrastructure.dto;
 import lombok.Builder;
 import lombok.Data;
 import software.sigma.sip.domain.entity.User;
+import software.sigma.sip.domain.entity.Wallet;
+
+import java.util.List;
 
 @Data
 @Builder
@@ -15,6 +18,7 @@ public class UserDto {
    private String country;
    private String birthDate;
    private String isActive;
+   private List<Wallet> walletList;
 
    public static UserDto toUserDto(User user) {
       return UserDto.builder().id(user.getId())
@@ -24,10 +28,11 @@ public class UserDto {
               .phone(user.getPhone())
               .country(user.getCountry())
               .birthDate(user.getBirthDate())
-              .isActive(user.getIsActive()).build();
+              .isActive(user.getIsActive())
+              .walletList(user.getWalletList()).build();
    }
 
    public User toUser() {
-      return new User(id, name, surname, email, phone, country, birthDate, isActive);
+      return new User(id, name, surname, email, phone, country, birthDate, isActive, walletList);
    }
 }
