6f4807f187493db1441b225dc73ce9bd5e4becc3
Roman Dovzhenko <dovzhenko.roman.kp03@gmail.com>
2022-08-27 13:01:18 +0300

    SIP-40: Add endpoint to find wallets by user id. Add method to return List<Wallet> in WalletService. Add method in WalletRepository.

--- src/main/java/software/sigma/sip/domain/repository/WalletRepository.java
+++ src/main/java/software/sigma/sip/domain/repository/WalletRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import software.sigma.sip.domain.entity.Wallet;
 
+import java.util.List;
+
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
+   List<Wallet> findWalletsByOwnerId(Long ownerId);
 }
